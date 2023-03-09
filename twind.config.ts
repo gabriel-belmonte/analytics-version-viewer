88cf76eb44e78eecbeb33c2438e25fa38d6cc697
Gabriel Belmonte <gabriel.belmonte@cbsinteractive.com>
2023-03-09 15:18:33 +0100

    version v1.6

--- twind.config.ts
+++ twind.config.ts
@@ -2,4 +2,18 @@ import { Options } from "$fresh/plugins/twind.ts";
 
 export default {
   selfURL: import.meta.url,
+  plugins: {
+    "scrollbar-hide": {
+      /* IE and Edge */
+      "-ms-overflow-style": "none",
+
+      /* Firefox */
+      "scrollbar-width": "none",
+
+      /* Safari and Chrome */
+      "&::-webkit-scrollbar": {
+        display: "none",
+      },
+    },
+  },
 } as Options;
